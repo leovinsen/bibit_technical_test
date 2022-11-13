ef5bda1e45b126431556caee8ebe5ff50779afe1
Leonardo Vinsen <leonardovinsen@gmail.com>
2022-11-13 22:06:47 +0700

    feat: setup alarm package in main application

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -1,9 +1,23 @@
+import 'package:alarm/alarm.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'pages/create_alarm/create_alarm_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // initialize database
+  final database =
+      await $FloorAppDatabase.databaseBuilder('alarm_app.db').build();
+
+  runApp(
+    // provide AlarmRepository to the whole app, as it will be used in all pages.
+    RepositoryProvider(
+      create: (_) => LocalAlarmRepository(database.alarmDao),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
